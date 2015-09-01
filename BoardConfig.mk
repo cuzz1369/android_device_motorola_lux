180a49f90a4f36559535d38b097280657d08f99d
Louis Popi <theh2o64@gmail.com>
2015-09-01 11:33:53 +0200

    lux : Specify the defconfig and add required mkbootimg offset configuration

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,9 +53,11 @@ BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
-BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
-BOARD_RAMDISK_OFFSET     := 0x02000000
+BOARD_KERNEL_TAGS_OFFSET := 0x01E00000 # For CAF
+BOARD_RAMDISK_OFFSET     := 0x02000000 # For CAF
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01E00000 # TODO: Need to be check on stock firmware
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8939
+TARGET_KERNEL_CONFIG := lux_defconfig
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 TARGET_KERNEL_HEADER_ARCH := arm64
